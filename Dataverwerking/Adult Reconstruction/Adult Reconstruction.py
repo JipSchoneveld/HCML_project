8d25706a41ab05ce5b83089dd7d12536d441781f
dibbi2001 <y_dibbits@hotmail.com>
2024-06-10 14:52:49 +0200

    read file function

--- Dataverwerking/Adult Reconstruction/Adult Reconstruction.py
+++ Dataverwerking/Adult Reconstruction/Adult Reconstruction.py
@@ -1,13 +1,19 @@
 import pandas as pd
-
+import numpy as np
+import matplotlib.pyplot as plt
 #read in
-adult_reconstructed = pd.read_csv('adult_reconstruction.csv')
-# print(adult_reconstructed[['gender']])
+def read_file(csv_file):
+    df = pd.read_csv(csv_file)
+    return df
+
+adult_reconstructed = read_file('adult_reconstruction.csv')
+print(adult_reconstructed[['gender']])
 
 # #explore
-# print(adult_reconstructed[['marital-status', 'gender']].value_counts())
+print(adult_reconstructed[['marital-status', 'gender']].value_counts())
+print(adult_reconstructed[['education', 'gender']].value_counts())
 # print(adult_reconstructed[['income', 'gender']].value_counts())
-# print(adult_reconstructed[['race', 'gender']].value_counts())
+print(adult_reconstructed[['race', 'gender']].value_counts())
 # print(adult_reconstructed[['age', 'gender']].value_counts())
 
 number_of_rows = adult_reconstructed.shape[0]
@@ -53,12 +59,37 @@ race_income_reconstructed = adult_reconstructed[['race', 'high_income']].value_c
 high_income_white_reconstructed = 100 * race_income_reconstructed[1]/race_reconstructed[0]
 high_income_black_reconstructed = 100 * race_income_reconstructed[4]/race_reconstructed[1]
 
+#education and income
+education_income_reconstructed = adult_reconstructed[['education', 'high_income']].value_counts()
+
+#create bar graphs
+
+age_range = pd.Series(index=range(91), dtype=int)
+age_reconstructed = age_reconstructed.reindex(age_range.index, fill_value=0)
+age_reconstructed_non_zero = age_reconstructed[age_reconstructed > 0]
+
+age_reconstructed_non_zero.plot(kind= 'bar')
+plt.title('Age Distribution')
+plt.xlabel('Age')
+plt.ylabel('Frequency')
+plt.xticks(rotation=0)
+
+education_income_reconstructed.plot(kind='bar')
+plt.title('Education and Income')
+
+
+
+plt.show()
+
 #print print print
 print(adult_reconstructed.head())
 print(percentage_women_reconstructed)
 print(percentage_men_reconstructed)
 print(ms_income_reconstructed)
+print(education_income_reconstructed)
 print(high_income_female_reconstructed)
 print(high_income_male_reconstructed)
 print(high_income_black_reconstructed)
 print(high_income_white_reconstructed)
+
+
